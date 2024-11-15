b0f056246d686c24d9a605539741b52ec966de92
Juan Manuel Cuellar <juan.cuellar@becallgroup.com>
2024-11-15 15:11:23 +0000

    Update Login.php

--- app/Livewire/Login.php
+++ app/Livewire/Login.php
@@ -60,37 +60,37 @@ class Login extends Component
                 $API_USER,
                 $API_PASS
             ];
-            // Realizamos la petición
-            // $response = $client->post($API_URL, [
-            //     'json' => $body,
-            //     'auth' => $auth,
-            //     'verify' => false,
-            //     'timeout' => 10
-            // ]);
+            Realizamos la petición
+            $response = $client->post($API_URL, [
+                'json' => $body,
+                'auth' => $auth,
+                'verify' => false,
+                'timeout' => 10
+            ]);
+            Decodificamos la respuesta
+            $responseStatus = $response->getStatusCode();
+            $reponseOk = $response->getReasonPhrase();  
+            $responseBody = json_decode($response->getBody());
+            Si la respuesta es fallida
+            if($responseStatus != 200 || $reponseOk != 'OK' || $responseBody->grupo != 'True'){
+                return session()->flash('error', 'Usuario o contraseña incorrectos');
+            }
+            Si es exitosa sacamos la información del usuario
+            $body = [
+                'token' => $API_TOKEN,
+                'user' => $valid_username,
+            ];
+            $response = $client->post($API_INFO_URL, [
+                'json' => $body,
+                'auth' => $auth,
+                'verify' => false,
+                'timeout' => 10
+            ]);
             // Decodificamos la respuesta
-            // $responseStatus = $response->getStatusCode();
-            // $reponseOk = $response->getReasonPhrase();  
-            // $responseBody = json_decode($response->getBody());
-            // Si la respuesta es fallida
-            // if($responseStatus != 200 || $reponseOk != 'OK' || $responseBody->grupo != 'True'){
-            //     return session()->flash('error', 'Usuario o contraseña incorrectos');
-            // }
-            // Si es exitosa sacamos la información del usuario
-            // $body = [
-            //     'token' => $API_TOKEN,
-            //     'user' => $valid_username,
-            // ];
-            // $response = $client->post($API_INFO_URL, [
-            //     'json' => $body,
-            //     'auth' => $auth,
-            //     'verify' => false,
-            //     'timeout' => 10
-            // ]);
-            // Decodificamos la respuesta
-            // $responseBody = json_decode($response->getBody());
-            $email = /*$responseBody->email; */ "juan.cuellar@becallgroup.com";
+            $responseBody = json_decode($response->getBody());
+            $email = $responseBody->email;
             // Sacamos todos los grupos del usuario
-            $grupos = /* $responseBody->grupo; */ "peoplecalladmin,peoplecall";
+            $grupos = $responseBody->grupo;
             $grupos = explode(',', $grupos);
             $isAdmin = False;
             // Si el usuario tiene el grupo de peoplecalladmin le ponemos "admin"
@@ -98,7 +98,7 @@ class Login extends Component
                 $isAdmin = True;
             }
             // Buscamos al usuario en la base de datos
-            $user = User::where('name', /* $valid_username*/ "juan.cuellar")->first();
+            $user = User::where('name', $valid_username)->first();
             // Si el usuario no existe lo creamos
             if(!$user){
                 $user = new User();
